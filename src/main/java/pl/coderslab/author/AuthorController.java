aa3866064414f79a786335fc0b934e4d15cc6f1b
Przemyslaw Solski <psolski@poczta.onet.pl>
2019-10-28 14:17:52 +0100

    Author + Dao; Publisher + Dao

---
+++ src/main/java/pl/coderslab/author/AuthorController.java
@@ -0,0 +1,49 @@
+package pl.coderslab.author;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/authors")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/add")
+    @ResponseBody
+    public String add(){
+        Author author = new Author();
+        author.setFirstName("Jan");
+        author.setLastName("Kowalski");
+        authorService.create(author);
+        return "Author added id= " + author.getId();
+    }
+
+    @GetMapping("/find/{id}")
+    @ResponseBody
+    public String find(@PathVariable Long id){
+        Author author = authorService.findOne(id);
+        if(author!=null) {
+            return author.toString();
+        }
+        return "Author not found";
+    }
+
+    @GetMapping("/update")
+    @ResponseBody
+    public String update(@PathVariable Long id) {
+        Author author = authorService.findOne(id);
+        author.setFirstName("nowy!!!!");
+        authorService.update(author);
+        return "Book updated, id= " + author.getId();
+    }
+}
